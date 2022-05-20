5c3f6be55c9ea2a84110e11b13c10840f6160a77
abdelfatah2000 <abdelfattahmohamed596@gmail.com>
2022-05-20 02:41:40 +0200

    email

--- Project/core/settings.py
+++ Project/core/settings.py
@@ -83,8 +83,8 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abdelfattahmohamed960@gmail.com'
-EMAIL_HOST_PASSWORD = '01200186617amh'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
