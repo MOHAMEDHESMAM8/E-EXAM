fa76d13357a9749d753eb1debd92a6f32bebec78
abdelfatah2000 <abdelfattahmohamed596@gmail.com>
2022-05-20 02:51:10 +0200

    database

--- Project/core/settings.py
+++ Project/core/settings.py
@@ -67,20 +67,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'e-exam',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': ''
-#     }
-# }
-
-import dj_database_url
 DATABASES = {
-    'default' : dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e-exam',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': ''
+    }
 }
+
+# import dj_database_url
+# DATABASES = {
+#     'default' : dj_database_url.config(conn_max_age=600)
+# }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
